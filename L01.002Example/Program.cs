6fab7c0627b6f146e47fcfbb7b02a43295ddd683
Mipusatina <mipspam2@mail.ru>
2023-01-29 20:31:27 +0100

    002 und 003

---
+++ L01.002Example/Program.cs
@@ -0,0 +1,4 @@
+int numberA = new Random().Next(1,10); //random 1 to 9
+Console.Write(numberA+" + ");
+int numberB = new Random().Next(1,10); //random 1 to 9
+Console.Write(numberB+" = "+(numberA+numberB));
---
+++ L01.003HalloLove/Program.cs
@@ -0,0 +1,9 @@
+Console.Write("Input name: ");
+string name (Console.ReadLine);
+if(name.ToLower() == "masha"){
+    Console.WriteLine("Huraa! Etoge Masha!!!");
+} else {
+    Console.WriteLine("Hallo, "+name);
+}
+int numberB = new Random().Next(1,10); //random 1 to 9
+Console.Write(numberB+" = "+(numberA+numberB));
