39d67a418c482f657c2864cee0201900144807be
Mipusatina <mipspam2@mail.ru>
2023-02-15 17:55:16 +0100

    Try with gitignore in folder

---
+++ L03.002SortArray/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[] arr = {1,4,3,2,6,7,1,1,2,3,4,5,6};
+
+void PrintArray(int[] array){
+    int length = array.Length;
+    for(int i=0; i<length; i++){
+        System.Console.Write($"{array[i]}");
+        System.Console.Write(" ");
+    }
+    System.Console.WriteLine();
+}
+
+void SelectionSort(int[] array){
+    for(int i = 0; i < array.Length-1; i++){
+        int minPosition = i;
+
+        for(int j = i+1; j < array.Length; j++){
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary=array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
