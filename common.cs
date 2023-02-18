008ce1f3925bba64d01a95008fa1f9046d025f5a
Mipusatina <mipspam2@mail.ru>
2023-02-18 19:00:20 +0100

    common, 2x Array

--- common.cs
+++ common.cs
@@ -65,3 +65,51 @@ string DecToBin(int num){
     return ""+DecToBin(num/2)+num%2;
 
 }
+
+//Генераци заполенного 2х мерного массива нужного размера
+int[,] Generate2xArray(int height, int length, int minValue, int maxValue)
+{
+    int[,] array = new int[height, length];
+    Random rnd = new Random();
+    for (int j = 0; j < height; j++)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            array[j, i] = rnd.Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+//Печать массива 2х мерного массива
+void Print2xArray(int[,] array)
+{
+    int maxCharsInElement = 4;
+    System.Console.WriteLine("Печать массива:");
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        System.Console.Write("[");
+        for (int i = 0; i < array.GetLength(1) - 1; i++)
+        {
+            System.Console.Write(
+                TabulationText(
+                    array[j, i].ToString(), 
+                    maxCharsInElement)
+                 + ", ");
+        }
+        System.Console.WriteLine(
+            TabulationText(
+                array[j, array.GetLength(1) - 1].ToString(), 
+                maxCharsInElement)
+            + "]");
+    }
+}
+
+//Доведение строки до размера int maxCharsInElement
+string TabulationText(string text, int maxCharsInElement)
+{
+    for(int weHave = text.Length; weHave < maxCharsInElement; weHave++){
+    text = " " + text;
+    }
+return text;
+}
